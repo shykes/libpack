14497943da1bcf1e1b5d242a4fb39a11e78b8bb3
Solomon Hykes <solomon@docker.com>
2014-06-09 17:09:34 +0000

    Inject objects straight to git DB. No tree or commit objects yet.
    Signed-off-by: Solomon Hykes <solomon@docker.com>

--- fsgit.go
+++ fsgit.go
@@ -1,35 +1,52 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha1"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"path"
+	"strings"
 
 	"github.com/dotcloud/docker/vendor/src/code.google.com/p/go/src/pkg/archive/tar"
 )
 
 func main() {
-	result, err := tar2git(os.Stdin, "")
+	result, err := tar2git(os.Stdin, os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(result)
 }
 
+func git(repo string, stdin io.Reader, args ...string) (string, error) {
+	cmd := exec.Command("git", append([]string{"--git-dir", repo}, args...)...)
+	if stdin != nil {
+		cmd.Stdin = stdin
+	}
+	out, err := cmd.Output()
+	return string(out), err
+}
+
+func gitHashObject(repo string, src io.Reader) (string, error) {
+	out, err := git(repo, src, "hash-object", "-w", "--stdin")
+	if err != nil {
+		return "", err
+	}
+	return strings.Trim(string(out), " \t\r\n"), nil
+}
+
 // tar2git decodes a tar stream from src, then encodes it into a new git commit
 // such that the full tar stream can be reconsistuted from the git data alone.
 // It retusn hash of the git commit, or an error if any.
 func tar2git(src io.Reader, repo string) (hash string, err error) {
-	// FIXME: write straight to the git object filesystem
-	tmp, err := ioutil.TempDir("", "tmp")
-	if err != nil {
+	if _, err := git(repo, nil, "init", "--bare", repo); err != nil {
 		return "", err
 	}
-	hash = tmp
+	tree := make(map[string]string)
 	tr := tar.NewReader(src)
 	for {
 		hdr, err := tr.Next()
@@ -40,35 +57,41 @@ func tar2git(src io.Reader, repo string) (hash string, err error) {
 			return "", err
 		}
 		fmt.Printf("[META] %s\n", hdr.Name)
-		metaDst := path.Join(tmp, "_fs_meta", fmt.Sprintf("%0x", sha1.Sum([]byte(hdr.Name))))
-		fmt.Printf("    ---> storing metadata in %s\n", metaDst)
-		if err := os.MkdirAll(path.Dir(metaDst), 0700); err != nil {
-			return "", err
-		}
-		metaFile, err := os.OpenFile(metaDst, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0700)
+		metaBlob, err := headerReader(hdr)
 		if err != nil {
 			return "", err
 		}
-		metaWriter := tar.NewWriter(metaFile)
-		if err := metaWriter.WriteHeader(hdr); err != nil {
+		metaHash, err := gitHashObject(repo, metaBlob)
+		if err != nil {
 			return "", err
 		}
-		metaWriter.Close()
+		metaDst := path.Join("_fs_meta", fmt.Sprintf("%0x", sha1.Sum([]byte(hdr.Name))))
+		fmt.Printf("    ---> storing metadata in %s\n", metaDst)
+		tree[metaDst] = metaHash
 		// FIXME: git can carry symlinks as well
 		if hdr.Typeflag == tar.TypeReg {
 			fmt.Printf("[DATA] %s %d bytes\n", hdr.Name, hdr.Size)
-			dataDst := path.Join(tmp, "_fs_data", hdr.Name)
-			if err := os.MkdirAll(path.Dir(dataDst), 0700); err != nil {
-				return "", err
-			}
-			dataFile, err := os.OpenFile(dataDst, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0700)
+			dataHash, err := gitHashObject(repo, tr)
 			if err != nil {
 				return "", err
 			}
-			if _, err := io.Copy(dataFile, tr); err != nil {
-				return "", err
-			}
+			dataDst := path.Join("_fs_data", hdr.Name)
+			tree[dataDst] = dataHash
 		}
 	}
+	fmt.Printf("Ready to write %d items to new tree:\n", len(tree))
+	for key, hash := range tree {
+		fmt.Printf("    %s  %s\n", hash, key)
+	}
 	return
+}
+
+func headerReader(hdr *tar.Header) (io.Reader, error) {
+	var buf bytes.Buffer
+	w := tar.NewWriter(&buf)
+	defer w.Close()
+	if err := w.WriteHeader(hdr); err != nil {
+		return nil, err
+	}
+	return &buf, nil
 }
