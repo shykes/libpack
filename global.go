552dfc8f7310b54eeb2f4c7d8db6ee9c9cf2cf93
Solomon Hykes <solomon@docker.com>
2014-10-23 20:13:14 +0000

    Remove unfinished types to clarify what works today.
    Signed-off-by: Solomon Hykes <solomon@docker.com>

--- global.go
+++ global.go
@@ -1,128 +1,13 @@
 package libpack
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"path"
 	"strconv"
 	"strings"
 
 	git "github.com/libgit2/git2go"
 )
-
-type GlobalTree struct {
-	db *DB
-}
-
-func (g *GlobalTree) Get(key string) (string, error) {
-	return g.db.Scope("data").Get(key)
-}
-
-func (g *GlobalTree) List(key string) ([]string, error) {
-	return g.db.Scope("data").List(key)
-}
-
-func (g *GlobalTree) Dump(w io.Writer) error {
-	return g.db.Scope("data").Dump(w)
-}
-
-func InitGlobal(repo, ref string) (*GlobalTree, error) {
-	db, err := Init(repo, ref)
-	if err != nil {
-		return nil, err
-	}
-	return &GlobalTree{db}, nil
-}
-
-func OpenGlobal(repo, ref string) (*GlobalTree, error) {
-	db, err := Open(repo, ref)
-	if err != nil {
-		return nil, err
-	}
-	return &GlobalTree{db}, nil
-}
-
-func (g *GlobalTree) ListMounts() ([]*Mount, error) {
-	var mounts []*Mount
-	err := walkAnnotations(g.db.Scope("mounts"), func(target, data string) {
-		fmt.Printf("found mount annotation for %s\n", target)
-		mnt, err := parseMount(data)
-		if err != nil {
-			// Ignore invalid mount entries
-			return
-		}
-		mounts = append(mounts, mnt)
-	})
-	if err != nil {
-		return nil, err
-	}
-	return mounts, nil
-}
-
-func (g *GlobalTree) LoadMount(mnt *Mount) error {
-	err := setAnnotation(g.db.Scope("mounts"), mnt.Dst, mnt.String())
-	if err != nil {
-		return err
-	}
-	return g.db.Commit(fmt.Sprintf("LoadMount %s %s", mnt.Dst, mnt.Src))
-}
-
-func (g *GlobalTree) Mount(dst string) error {
-	mnt, err := g.getMount(dst)
-	if err != nil {
-		return err
-	}
-	if err = g.db.Scope("data").Add(mnt.Dst, mnt.Src); err != nil {
-		return err
-	}
-	return g.db.Commit(fmt.Sprintf("mount %s", dst))
-}
-
-func (g *GlobalTree) getMount(dir string) (*Mount, error) {
-	blob, err := g.db.Scope("mounts").Get(MkAnnotation(dir))
-	if err != nil {
-		return nil, err
-	}
-	return parseMount(blob)
-}
-
-type Mount struct {
-	Dst string
-	Src *git.Oid
-}
-
-type mountSchema struct {
-	Dst string `json:"dst"`
-	// FIXME: symbolic reference (mount a name instead of a hash)
-	Src string `json:"src"`
-	// FIXME: signature
-}
-
-func (mnt *Mount) String() string {
-	s, _ := json.Marshal(&mountSchema{
-		Src: mnt.Src.String(),
-		Dst: mnt.Dst,
-	})
-	return string(s)
-}
-
-func parseMount(data string) (*Mount, error) {
-	var s mountSchema
-	err := json.Unmarshal([]byte(data), &s)
-	if err != nil {
-		return nil, err
-	}
-	dst := TreePath(s.Dst)
-	src, err := git.NewOid(s.Src)
-	if err != nil {
-		return nil, err
-	}
-	return &Mount{
-		Dst: TreePath(dst),
-		Src: src,
-	}, nil
-}
 
 func getAnnotation(db *DB, name string) (string, error) {
 	return db.Get(MkAnnotation(name))
