76f7343c0551d2685126d1878ae3050068e9d287
Solomon Hykes <solomon@docker.com>
2015-01-01 04:15:18 +0000

    'pack serve' and 'pack query'
    Signed-off-by: Solomon Hykes <solomon@docker.com>

--- pack/pack.go
+++ pack/pack.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/docker/libpack"
 )
@@ -14,8 +15,27 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Listening on tcp://0.0.0.0:4242\n")
-	if err := repo.ListenAndServe("tcp", "0.0.0.0:4242"); err != nil {
-		log.Fatal(err)
+	var cmd string
+	if len(os.Args) >= 2 {
+		cmd = os.Args[1]
+	} else {
+		cmd = "serve"
+	}
+
+	switch cmd {
+	case "serve":
+		{
+			fmt.Printf("Listening on tcp://0.0.0.0:4242\n")
+			if err := repo.ListenAndServe("tcp", "0.0.0.0:4242"); err != nil {
+				log.Fatal(err)
+			}
+		}
+	case "query":
+		{
+			p := libpack.NewPipeline(repo)
+			if err := p.Communicate(os.Stdin, os.Stdout, os.Stderr); err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 }
